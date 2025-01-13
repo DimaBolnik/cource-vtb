d23949ae3a9a28333dd8c6891e2f92876c210a5a
User <dimabolnik@mail.ru>
2025-01-13 14:40:12 +0300

    lesson7(dz)

---
+++ src/main/java/ru/dev/bolnik/dz7/Car.java
@@ -0,0 +1,64 @@
+package ru.dev.bolnik.dz7;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class Car implements Runnable {
+    private static int CARS_COUNT;
+    private static boolean win = true;
+    private final static Object lock = new Object();
+
+    static {
+        CARS_COUNT = 0;
+    }
+
+    private Race race;
+    private int speed;
+    private String name;
+    private CyclicBarrier cyclicBarrier;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public Car(Race race, int speed, CyclicBarrier cyclicBarrier) {
+        this.race = race;
+        this.speed = speed;
+        CARS_COUNT++;
+        this.name = "Участник #" + CARS_COUNT;
+        this.cyclicBarrier = cyclicBarrier;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println(this.name + " готовится");
+            Thread.sleep(500 + (int) (Math.random() * 800));
+            System.out.println(this.name + " готов");
+            cyclicBarrier.await();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < race.getStages().size(); i++) {
+            race.getStages().get(i).go(this);
+        }
+        synchronized (lock) {
+            if (this.win) {
+                System.out.println(this.name + " ПОБЕДИЛ!!!");
+                this.win = false;
+            }
+        }
+        try {
+            cyclicBarrier.await();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (BrokenBarrierException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
